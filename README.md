0e2b0d75cb6d54d901685ac18bca683f68556277
Raphyah <leonardosolimenoluiz@gmail.com>
2024-02-28 04:37:37 -0300

    Initial commit
    Committed all the files of this project.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # BinarySearch
- Simple binary search library
+
+This library implements an easy-to-use binary search function that can be used to find elements in sorted arrays.
+
+## Installation
+
+Here is how to install the module to your projects:
+
+### ES6 module
+
+This is the recommended way as it leverages a modern and elegant approach to importing modules, which is supported in all actively maintained JavaScript environments.
+
+```js
+import BinarySearch from './path/to/BinarySearch.mjs';
+```
+
+### Browsers
+
+```html
+<script src = "./path/to/BinarySearch.min.js"></script>
+```
+
+### Common.js
+
+```js
+const BinarySearch = require('./path/to/BinarySearch.min.js');
+```
+
+### AMD
+
+```js
+requirejs(['./path/to/BinarySearch.min.js'], function(BinarySearch) {
+  // ...
+});
+```
+
+## Usage
+
+After importing it you can start to use BinarySearch.
+The function `BinarySearch` should never be constructed or invoked, instead check it's static methods:
+
+```js
+const users = [
+  { name: 'Marie', age: 26 },
+  { name: 'Alexander', age: 31 },
+  { name: 'Natan', age: 27 },
+  { name: 'Eleanor', age: 33 },
+];
+
+users.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  } else if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+});
+
+console.log('Alexander is located at index:', BinarySearch.getIndex(users, v => v.name, 'Alexander'));
+console.log("Eleanor's age is:", BinarySearch.getValue(users, v => v.name, 'Eleanor').age);
+```
---
+++ build/BinarySearch.js
@@ -0,0 +1,82 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.BinarySearch = factory());
+})(this, (function () { 'use strict';
+
+  /**
+   * BinarySearch - Simple implementation of binary search for arrays
+   * Version: 1.0
+   * Author: Raphyah
+   * License: GNU General Public License version 3
+   * GitHub: https://www.github.com/Raphyah
+   *
+   * Description:
+   * This library implements an easy-to-use binary search function that can be used to find elements in sorted arrays.
+   * It uses a callback function to retrieve properties from the element before doing any comparison check.
+   *
+   * Examples can be found on my GitHub.
+   * 
+   * Last updated: February 28, 2024
+   * Release date: February 28, 2024
+   */
+
+  /**
+   * This class provides static methods for performing binary search operations on arrays.
+   * This class should not be invoked or constructed. Use its static methods instead.
+   */
+  function BinarySearch() {
+    throw new Error("BinarySearch cannot be used as function or constructor, use its static properties and methods instead.");
+  }
+
+  /**
+   * Performs a binary search to find the index of the specified item in the array.
+   * @param {any[]} arr - The array to be iterated.
+   * @param {(element: any) => any} property - A callback used to extract a value from each element.
+   * @param {any} find - The target element to search for.
+   * @returns {number} - The index of the target element, or -1 if not found.
+   */
+  BinarySearch.getIndex = function (arr, property, find) {
+    if (!arr || arr.constructor !== Array) {
+      throw new TypeError(`'BinarySearch.getIndex(arr, property, find)' expected the value of 'arr' to be an array, but instead ${arr && arr.constructor.name} was found.`);
+    }
+    if (!property || property.constructor !== Function) {
+      throw new TypeError(`'BinarySearch.getIndex(arr, property, find)' expected the value of 'property' to be a function, but instead ${property && property.constructor.name} was found.`);
+    }
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+
+      const PROPERTY = property(arr[mid]);
+
+      if (PROPERTY === find) {
+        return mid;
+      } else if (PROPERTY < find) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  };
+
+  /**
+   * Performs a binary search to find the value of the specified item in the array.
+   * @param {any[]} arr - The array to be iterated.
+   * @param {(element: any) => any} property - A callback used to extract a value from each element.
+   * @param {any} find - The target element to search for.
+   * @returns {any} - The value of the target element, or undefined if not found.
+   */
+  BinarySearch.getValue = function (arr, property, find) {
+    const INDEX = BinarySearch.getIndex(arr, property, find);
+    if (INDEX === -1) {
+      return undefined;
+    }
+    return arr[INDEX];
+  };
+
+  return BinarySearch;
+
+}));
